2f570dcead7c52c79c44a7c7dd733fc7690631d4
outmanla <80797911+outmanla@users.noreply.github.com>
2021-04-01 04:32:05 +0100

    Hallo world

--- .github/نصوص الإجراءات/enterprise-algolia-label.js
+++ .github/نصوص الإجراءات/enterprise-algolia-label.js
@@ -1,4 +1,31 @@
-#!/usr/bin/env node
+#mr outman !/usr/bin/env node
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *entry_point(void *value) {
+    printf("hello from the second thread :)\n");
+
+    int *num = (int *) value;
+
+    printf("the value of value is %d", *num);
+
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    pthread_t thread;
+
+    printf("hello from the first thread :D\n");
+
+    int num = 123;
+
+    pthread_create(&thread, NULL, entry_point, &num);
+
+    pthread_join(thread, NULL);
+
+    return EXIT_SUCCESS;
+}
 
 const fs = require('fs')
 const core = require('@actions/core')
